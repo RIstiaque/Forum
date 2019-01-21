8d5e0d8404c3327f99580a273a25d70f6064d1e1
RIstiaque <rifatul.istiaque@gmail.com>
2019-01-21 15:00:04 -0500

    initial commit

---
+++ app/Http/Controllers/RepliesController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thread;
+use App\Reply;
+
+class RepliesController extends Controller
+{
+    /**
+     * Create a new RepliesController instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Persist a new reply.
+     *
+     * @param  integer $channelId
+     * @param  Thread  $thread
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store($channelId, Thread $thread)
+    {
+        $this->validate(request(), ['body' => 'required']);
+
+        $thread->addReply([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]);
+
+        return back()
+            ->with('flash', 'Your reply has been left');
+    }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+
+        $reply->delete();
+
+        if (request()->expectsJson()){
+            return response(['status' => 'Reply deleted']);
+        }
+
+        return back();
+    }
+
+    public function update(Reply $reply)
+    {
+        $this->authorize('update', $reply);        
+
+        $reply->update(request(['body']));
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use App\Channel;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \View::composer('*', function ($view) {
+            $view->with('channels',  Channel::all());
+        });
+
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
+    }
+}
